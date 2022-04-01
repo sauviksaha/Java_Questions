cae35a12a3c223311038d4b2cdcbfcfc49cb0ea0
Sauvik Saha <86285663+sauviksaha@users.noreply.github.com>
2022-04-01 23:16:56 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Sum
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number:");
+        int n = sc.nextInt();
+        sc.close();
+        int num = n;
+        int sum = 0;
+
+        while(n > 0)
+        {
+            int d = n % 10;
+            sum = sum + d;
+            n = n / 10;
+        }
+        System.out.println("Sum of the digits of " + num + " = " + sum);
+    }
+}
